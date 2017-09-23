c60863df7f3cd0890b0e47d70674575ac1f76bdc
Luthfir <luthfirrahman04@gmail.com>
2017-09-23 10:44:30 +0700

    first commit'

---
+++ server/server.js
@@ -0,0 +1,33 @@
+var loopback = require('loopback');
+var boot = require('loopback-boot');
+
+var app = module.exports = loopback();
+
+// Bootstrap the application, configure models, datasources and middleware.
+// Sub-apps like REST API are mounted via boot scripts.
+boot(app, __dirname);
+
+app.start = function () {
+  // start the web server
+  return app.listen(function () {
+    app.emit('started');
+    console.log('Web server listening at: %s', app.get('url'));
+  });
+};
+
+
+if (require.main === module) {
+  app.io = require('socket.io')(app.start());
+  app.io.on('connection', function (socket) {
+    console.log('a user connected');
+
+    socket.on('chat message', function (msg) {
+      console.log('message: ', msg );
+      app.io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', function () {
+      console.log('user disconnected');
+    });
+  });
+}
